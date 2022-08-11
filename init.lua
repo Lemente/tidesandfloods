90317fa5f57c5a7de8866e17385c551d16d16428
Lemente <crafted.by.lemente@gmail.com>
2022-08-11 13:01:29 +0200

    added command "compare_block_status" for debugging

--- init.lua
+++ init.lua
@@ -41,3 +41,22 @@ minetest.register_chatcommand("sealevel", {
     end
   end
 })
+
+--if minetest.compare_block_status(cardinal_pos[i], "active") ~= true then
+
+
+minetest.register_chatcommand("compare_block_status", {
+    params = "<name> <X>,<Y>,<Z>",
+    description = "compare_block_status",
+    func = function(name, param)
+        local p = {}
+        local status
+        status, p.x, p.y, p.z = param:match(
+                "^([^ ]+) +([%d.-]+)[, ] *([%d.-]+)[, ] *([%d.-]+)$")
+        p = vector.apply(p, tonumber)
+        if status and p.x and p.y and p.z then
+            return minetest.chat_send_all(
+                tostring(status) .." at (" .. tostring(p.x) .. ", " .. tostring(p.y) .. ", " .. tostring(p.z) .. ") " .. " is " .. tostring(minetest.compare_block_status(p, tostring(status))))
+        end
+    end,
+})
